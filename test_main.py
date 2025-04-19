f05685067e166a4c6b47e737fff5c0a7c6deccf5
Brian Yoon <byoonie@gmail.com>
2025-04-19 00:08:35 -0700

    Refactor vfs and vfs test

--- test_main.py
+++ test_main.py
@@ -1,33 +1,20 @@
 import os
 import pytest
 
-from main import IGNORE_DIRS
-from main import is_valid_item
-from main import process_file
+from main import IGNORE_DIRS as PLEX_DIRS_LIST, is_valid_item
 
 FAKE_ROOT = "/V"
 
-PLEX_DIRS_LIST = IGNORE_DIRS
-
 FAKE_VIDEO_FILES_LIST = [
+    # "Abbott Elementary (2021) - S03E13.1080p.mkv"
     "Abbott.Elementary.S03E13.1080p.WEB.H264-SuccessfulCrab[TGx].mkv",
+    # "St. Denis Medical (2024) - S01E02.1080p.mkv"
     "St.Denis.Medical.S01E02.1080p.x265-ELiTE.mkv"
 ]
 
 FAKE_OTHER_FILES_LIST = [
     "_extract_srt_from_mkv.py",
     "_rename_files.py"
-]
-
-TEST_CASES = [
-    (
-        "Abbott.Elementary.S03E13.1080p.WEB.H264-SuccessfulCrab[TGx].mkv",
-        "Abbott Elementary (2021) - S03E13.1080p.mkv"
-    ),
-    (
-        "St.Denis.Medical.S01E02.1080p.x265-ELiTE.mkv",
-        "St. Denis Medical (2024) - S01E02.1080p.mkv"
-    )
 ]
 
 @pytest.fixture
@@ -37,35 +24,33 @@ def vfs(fs):
 
     # Populate with dirs used by Plex, which should be ignored by this script
     for dir_name in PLEX_DIRS_LIST:
-        fs.create_dir(FAKE_ROOT + "/" + dir_name)
+        fs.create_dir(os.path.join(FAKE_ROOT, dir_name))
 
     # TODO: Populate with fake video files inside folders to be processed
 
     # Populate with fake video files in root to be processed
     for video_filename in FAKE_VIDEO_FILES_LIST:
-        fs.create_file(FAKE_ROOT + "/" + video_filename)
+        fs.create_file(os.path.join(FAKE_ROOT, video_filename))
 
     # Populate with non-video files that should be ignored
     for other_filename in FAKE_OTHER_FILES_LIST:
-        fs.create_file(FAKE_ROOT + "/" + other_filename)
+        fs.create_file(os.path.join(FAKE_ROOT, other_filename))
 
     return fs
 
-def test_vfs_structure_is_correct(vfs):
+def test_vfs_contains_plex_dirs_and_files(vfs):
     # Populate set of expected fake root structure
-    expected_vfs_struct = set(IGNORE_DIRS)
-    expected_vfs_struct.update(set(FAKE_VIDEO_FILES_LIST))
-    expected_vfs_struct.update(set(FAKE_OTHER_FILES_LIST))
+    expected_vfs_struct = set(PLEX_DIRS_LIST) | set(FAKE_VIDEO_FILES_LIST) | set(FAKE_OTHER_FILES_LIST)
 
-    vfs_struct = os.listdir(FAKE_ROOT)
+    vfs_struct = set(os.listdir(FAKE_ROOT))
 
-    assert set(vfs_struct) == expected_vfs_struct
+    assert vfs_struct == expected_vfs_struct
 
 def test_is_valid_item():
-    for dir_name in IGNORE_DIRS:
+    for dir_name in PLEX_DIRS_LIST:
         assert not is_valid_item(dir_name)
 
 def test_valid_item_not_in_ignore_list():
-    assert is_valid_item("test_folder")
+    assert is_valid_item("some_random_folder")
 
 # TODO: Implement pyfakefs to imitate files/folders
